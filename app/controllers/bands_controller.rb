0300ce8347aab7540f9322289795e2929bb46c53
Kitagawa <140226k@gmail.com>
2024-04-11 17:22:02 +0900

    バンド分けの順番を修正

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -1,37 +1,40 @@
 class BandsController < ApplicationController
+  
   def index
     parts = Part.all
     song_parts = SongPart.all
     song_part_names = song_parts.pluck(:name)
-
+  
     # バンドの準備
     bands_1 = prepare_bands(parts, song_part_names)
     bands_2 = prepare_bands(parts, song_part_names)
-
-    @bands_1=bands_1
-    @bands_2=bands_2
-
+  
+    order = ["Lead", "top", "2nd", "3rd", "Bass", "V.P."]
+    @bands_1 = order.map { |part_name| [part_name, bands_1[part_name]] }.to_h
+    @bands_2 = order.map { |part_name| [part_name, bands_2[part_name]] }.to_h
+  
     @song_1 = Song.first
     @song_2 = Song.second
   end
+#    binding.pry
 
   private
 
   def prepare_bands(parts, song_part_names)
     # 各パートごとにニックネームをまとめる
     grouped_parts = parts.group_by { |part| part[:song_part_id_1] }
-
+  
     # バンドを準備
-    bands = {}
-
+    bands = HashWithIndifferentAccess.new # 順序付きのハッシュを作成
+  
     # パートごとにバンドを構築
     grouped_parts.each do |part, members|
       bands[song_part_names[part]] = members.map { |member| member[:nickname] }
     end
-
+  
     # 最大バンド数を取得
     max_band_count = grouped_parts.values.map(&:size).max
-
+  
     # 最大バンド数に満たない配列で、配列内の要素をコピーしてすべての配列の要素数を同じにする
     bands.each do |part, members|
       members.shuffle! # メンバーをランダムに並び替える
@@ -40,7 +43,7 @@ class BandsController < ApplicationController
         members << element_to_copy
       end
     end
-
+  
     bands
   end
 
--- app/models/song_part.rb
+++ app/models/song_part.rb
@@ -3,10 +3,10 @@ class SongPart < ActiveHash::Base
     { id: 0, name: '---' },
     { id: 1, name: 'Lead' },
     { id: 2, name: 'top' },
-    { id: 3, name: 'second' },
-    { id: 4, name: 'third' },
+    { id: 3, name: '2nd' },
+    { id: 4, name: '3rd' },
     { id: 5, name: 'Bass' },
-    { id: 6, name: 'percussion' },
+    { id: 6, name: 'V.P.' },
 ]
 
 include ActiveHash::Associations
