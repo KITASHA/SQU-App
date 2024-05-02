31b89ebc031daa38106a88b9075e02071e2a57c6
Kitagawa <140226k@gmail.com>
2024-05-02 14:47:33 +0900

    about/index/html.erbの編集

--- app/controllers/abouts_controller.rb
+++ app/controllers/abouts_controller.rb
@@ -1,6 +1,18 @@
 class AboutsController < ApplicationController
 
+  # SQUEAR概要説明ページ
   def index
   end
 
+  # 定期活動会についての詳細ページ
+  def show_1 
+  end
+
+  # Okayama Music SQUAREについての詳細ページ
+  def swow_2
+  end
+
+  # スターターバンド・アシスト制度についての詳細ページ
+  def swow_3
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   resources :parts 
   resources :bands
   resources :abouts, only: :index
+  get '/abouts/show_1', to: 'abouts#show_1', as: 'show_1_about'
+  get '/abouts/show_2', to: 'abouts#show_2', as: 'show_2_about'
+  get '/abouts/show_3', to: 'abouts#show_3', as: 'show_3_about'
   delete '/reset_all', to: 'application#reset_all'
 end
