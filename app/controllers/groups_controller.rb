3ae2c2285706a805da2dc9fb218c64690f7e248e
Kitagawa <140226k@gmail.com>
2024-08-04 04:36:22 +0900

    GroupsControllerのデバック

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -9,7 +9,7 @@ class GroupsController < ApplicationController
     song_parts = SongPart.all
     # SongPartのIDをキー、名前を値とするハッシュを作成
     song_part_names = song_parts.index_by(&:id).transform_values(&:name)
-
+    excluded_key = 0
     # グループの準備
     groups_1 = prepare_groups_1(parts, song_part_names)
     groups_2 = prepare_groups_2(parts, song_part_names)
@@ -32,50 +32,58 @@ class GroupsController < ApplicationController
     grouped_parts = parts.group_by(&:song_part_id_1)
     # 通常のハッシュを作成
     groups = {}
-
+  
     # パートごとにニックネームを収集
     grouped_parts.each do |part_id, members|
       groups[song_part_names[part_id]] = members.map(&:nickname)
     end
-
-    # 最大グループ数を取得
-    max_group_count = grouped_parts.values.map(&:size).max
-
+  
+    # 最大グループ数を取得（除外キーを考慮）
+    excluded_key = "---"
+    max_group_count = grouped_parts.reject { |key, _| song_part_names[key] == excluded_key }.values.map(&:size).max
+  
     # メンバーが不足しているグループを補充
     groups.each do |part, members|
+      next if part == excluded_key # 除外キーのグループをスキップ
+  
       members.shuffle!
       additional_members = members.cycle.take(max_group_count - members.size).shuffle
-      
+  
       additional_members.each do |member|
         members << member
       end
     end
   end
+  
 
   def prepare_groups_2(parts, song_part_names)
     # パートID2でグループ化
     grouped_parts = parts.group_by(&:song_part_id_2)
     # 通常のハッシュを作成
     groups = {}
-
+  
     # パートごとにニックネームを収集
     grouped_parts.each do |part_id, members|
       groups[song_part_names[part_id]] = members.map(&:nickname)
     end
-
-    # 最大グループ数を取得
-    max_group_count = grouped_parts.values.map(&:size).max
-
+  
+    # 最大グループ数を取得（除外キーを考慮）
+    excluded_key = "---"
+    max_group_count = grouped_parts.reject { |key, _| song_part_names[key] == excluded_key }.values.map(&:size).max
+  
     # メンバーが不足しているグループを補充
     groups.each do |part, members|
+      next if part == excluded_key # 除外キーのグループをスキップ
+  
       members.shuffle!
       additional_members = members.cycle.take(max_group_count - members.size).shuffle
-      
+  
       additional_members.each do |member|
         members << member
       end
     end
   end
+  
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
