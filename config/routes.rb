6a641749df612de41f5c82d83a78deef96f5a534
Kitagawa <140226k@gmail.com>
2024-12-04 19:18:36 +0900

    workshop

--- config/routes.rb
+++ config/routes.rb
@@ -21,5 +21,6 @@ Rails.application.routes.draw do
   get '/homes/show_3', to: 'homes#show_3', as: 'show_3_home'
   get '/homes/show_4', to: 'homes#show_4', as: 'show_4_home'
   get '/homes/option', to: 'homes#option', as: 'option_home'
+  get '/homes/workshop', to: 'homes#workshop', as: 'workshop_home'
   delete '/reset_all', to: 'application#reset_all'
 end
