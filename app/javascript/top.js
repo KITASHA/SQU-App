cc3a64f5b35402979c18877dfc46895028c1ccca
kitagawa <140226k@gmail.com>
2025-01-10 14:08:13 +0900

    余白

--- app/javascript/top.js
+++ app/javascript/top.js
@@ -1,18 +1,24 @@
-// ボタンがクリックされたときにtopFunctionを呼び出す
-document.getElementById("myBtn").addEventListener("click", topFunction);
+document.addEventListener("DOMContentLoaded", function() {
+  // スクロール位置が一定になるとボタンを表示
+  window.onscroll = function() {
+    let myBtn = document.getElementById("myBtn");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+      myBtn.style.display = "block"; // 100px以上スクロールされたら表示
+    } else {
+      myBtn.style.display = "none"; // それ以下なら非表示
+    }
+  };
 
-// スクロール位置が一定になるとボタンを表示
-window.onscroll = function() {
-  let myBtn = document.getElementById("myBtn");
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    myBtn.style.display = "block"; // 100px以上スクロールされたら表示
-  } else {
-    myBtn.style.display = "none"; // それ以下なら非表示
+  // ボタンがクリックされたときにtopFunctionを呼び出す
+  document.getElementById("myBtn").addEventListener("click", topFunction);
+
+  // ボタンがクリックされたらページの上に戻る
+  function topFunction() {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
   }
-};
+});
 
-// ボタンがクリックされたらページの上に戻る
-function topFunction() {
-  document.body.scrollTop = 0; // Safari
-  document.documentElement.scrollTop = 0; // Chrome, Firefox, IE
-}
+window.addEventListener("turbo:load", topFunction);
