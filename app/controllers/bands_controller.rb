27b126063855696a90bec23304fd43c7ba5685e8
kitagawa <140226k@gmail.com>
2025-03-19 02:58:54 +0900

    band

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -10,7 +10,7 @@ class BandsController < ApplicationController
   end
 
   def create
-    @band = current_user.bands.build(band_params)
+    @band = build(band_params)
 
     if @band.save
       redirect_to @band, notice: 'バンドが登録されました。'
