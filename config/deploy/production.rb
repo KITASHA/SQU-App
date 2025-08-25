ccc859e7864cfd8e948fa340a02116e4868b5469
kitagawa <140226k@gmail.com>
2025-08-26 02:18:10 +0900

    変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 server "54.249.128.34", user: "ubuntu", roles: %w{app db web}
 
 set :ssh_options, {
-  keys: %w(/home/kitagawa/.ssh/LightsailDefaultKey-ap-northeast-1.pem),
+  keys: %w(~/.ssh/LightsailDefaultKey-ap-northeast-1.pem),
   forward_agent: true,
   auth_methods: %w(publickey)
 }
