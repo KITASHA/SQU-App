dd22f9a2c311e125b0422c1d57fdcda75f1a30b9
kitagawa <140226k@gmail.com>
2025-08-27 01:21:47 +0900

    デプロイ

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,1 +1,5 @@
-server "54.249.128.34", user: "ubuntu", roles: %w{app db web}
+server '54.249.128.34', user: 'ubuntu', roles: %w{app db web}, ssh_options: {
+  keys: %w(~/.ssh/id_ed25519),
+  forward_agent: true,
+  auth_methods: %w(publickey)
+}
