a8e99b250c2a1ae1cd7fa03564053afe92213056
kitagawa <140226k@gmail.com>
2025-08-29 02:32:24 +0900

    デプロイ

--- config/puma.rb
+++ config/puma.rb
@@ -10,9 +10,10 @@ environment ENV.fetch("RAILS_ENV") { "production" }
 workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 preload_app!
 
-# shared 配下の UNIX ソケットにバインド（port 競合回避）
-app_dir = File.expand_path("../..", __FILE__)
-shared_dir = "#{app_dir}/shared"
+# shared ディレクトリの絶対パスを指定
+shared_dir = "/var/www/SQU-App/shared"
+
+# ソケット
 bind "unix://#{shared_dir}/tmp/sockets/puma.sock"
 
 # PID / state / log
@@ -25,31 +26,5 @@ on_worker_boot do
   ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
 end
 
-# tmp_restart プラグイン# shared ディレクトリの絶対パスを指定
-shared_dir = "/var/www/SQU-App/shared"
-
-# ソケット
-bind "unix://#{shared_dir}/tmp/sockets/puma.sock"
-
-# PID / state / log
-pidfile "#{shared_dir}/tmp/pids/puma.pid"
-state_path "#{shared_dir}/tmp/pids/puma.state"
-stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
-
-# スレッド数
-max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
-threads min_threads_count, max_threads_count
-
-# 環境
-environment ENV.fetch("RAILS_ENV") { "production" }
-
-# workers / preload_app!
-workers ENV.fetch("WEB_CONCURRENCY") { 2 }
-preload_app!
-
-on_worker_boot do
-  ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
-end
-
+# tmp_restart プラグイン
 plugin :tmp_restart
